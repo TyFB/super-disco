191cf3798d23b5eb3e98de4140acda30a84c213e
unknown <tfb1223@gmail.com>
2022-09-15 22:14:21 -0500

    finally works just need to figure out the present issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -99,50 +99,53 @@ function getDate() {
 };
 // save plans to localStorage
 function savePlans() {
-    //$(".container").data
+    //$("planDay").data(JSON.stringify(planDay))
     localStorage.setItem("planDay", JSON.stringify(planDay));
 }
 // get any plans saved in localStorage
 function init() {
     var savedPlans = JSON.parse(localStorage.getItem("planDay"));
+    console.log(savedPlans);
     if (savedPlans) {
         planDay = savedPlans
     }
     savePlans();
 }
 getDate();
+init();
 // Using jQuery, bootstrap, and moment to set up schedule
-planDay.forEach(function (thisPlan) {
+planDay.forEach(function (thisPlan, index) {
     var planRow = $('<form>').attr({ "class": "row" });
     $(".container").append(planRow);
+    
     var hourTime = $("<div>")
         .text(thisPlan.hour + thisPlan.sun)
         .attr({ "class": "col-md-2 hour" });
+    
     var hourPlan = $("<div>")
         .attr({ "class": "col-md-9 description p-0" });
     var planData = $("<textarea>");
+    planData.val(JSON.parse(localStorage.getItem("planDay"))[index].plan)
     hourPlan.append(planData);
     planData.attr("id", thisPlan.id);
-    if (thisPlan.time < moment().format("HH")) {
-        planData.attr({ "class": "past" })
-    } else if (thisPlan.time === moment().format("HH")) {
+    if (thisPlan.time == moment().format("HHmm")/*||thisPlan.time++ == moment().format("HHmm")*/) {
         planData.attr({ "class": "present" })
-    } else if (thisPlan.time > moment().format("HH")) {
+    } else if (thisPlan.time < moment().format("HHmm")) {
+        planData.attr({ "class": "past" })
+    } else if (thisPlan.time > moment().format("HHmm")) {
         planData.attr({ "class": "future" })
     }
-    var savePlan = $("<i class='far fa-save fa-lg'></i>")
+    var savePlan = $("<i class='far fa-solid fa-sd-card fa'></i>")
     var saveGame = $("<button>")
         .attr({ "class": "col-md-1 saveBtn" });
     saveGame.append(savePlan);
     planRow.append(hourTime, hourPlan, saveGame);
 });
-init();
 // when the save button is clicked it will be saved to the localStorage
-$(".saveBtn").on("click", function(event) {
-    event.preventDefault();
-    var savedLocal = $(this).parent(".description").children(".future").attr("id");
-    planDay[savedLocal] = $(this).parent(".description").children(".future").val();
-    console.log(savedLocal);
-   savePlans();
-   init();
+$(".saveBtn").click(function (e) {
+    e.preventDefault();
+    var savedLocal = $(this).siblings(".description").children("textarea").attr("id");
+    planDay[savedLocal].plan = $(this).siblings(".description").children("textarea").val();
+    savePlans();
 })
+//
